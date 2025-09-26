042e7287222ff1b2ab812484dbfc2df47b134050
dmitrelt <94909873+dmitrelt@users.noreply.github.com>
2025-09-26 14:46:32 +0000

    сделанная вторая лаба

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    char temp;
+    int i;
+    
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
 }
-
